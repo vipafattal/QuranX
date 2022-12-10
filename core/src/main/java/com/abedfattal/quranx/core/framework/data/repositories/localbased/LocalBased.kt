54baa31a7ae43d2b54a6e9025ae1d97aae7ecf89
abed.fattal@develab.io <abed.develab>
2022-12-10 22:50:35 +0300

    Revert "Improvment on LocalBased.kt"
    This reverts commit 1462ee78361841dc6c7d9180c72a4df7b7abedb1.

--- core/src/main/java/com/abedfattal/quranx/core/framework/data/repositories/localbased/LocalBased.kt
+++ core/src/main/java/com/abedfattal/quranx/core/framework/data/repositories/localbased/LocalBased.kt
@@ -1,72 +1,53 @@
 package com.abedfattal.quranx.core.framework.data.repositories.localbased
 
-import androidx.annotation.Keep
+import android.util.Log
 import com.abedfattal.quranx.core.model.ProcessState
-import com.abedfattal.quranx.core.utils.toSuccessProcess
-import kotlinx.coroutines.InternalCoroutinesApi
+import com.abedfattal.quranx.core.utils.onSuccess
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
 
-@Keep
 abstract class LocalBased internal constructor() {
 
 
-    protected inline fun <T : Any> caller(
-        prioritizeRemote: Boolean,
+    companion object {
+        @PublishedApi
+        internal const val REMOTE_CALL = "Remote call on:"
+
+        @PublishedApi
+        internal const val LOCAL_CALL = "Local call on:"
+    }
+
+    protected inline fun <reified T : Any> caller(
+        crossinline local: suspend () -> List<T>,
+        crossinline remote: () -> Flow<ProcessState<List<T>>>,
+        crossinline onRemoteSuccess: suspend (List<T>) -> Unit
+    ): Flow<ProcessState<List<T>>> = flow {
+        val localData = local.invoke()
+        if (localData.isNotEmpty())
+            emit(ProcessState.Success(localData))
+        else {
+            val remoteEditions = remote.invoke()
+            Log.d(REMOTE_CALL, remoteEditions::class.java.simpleName)
+            remoteEditions.onSuccess.collect { onRemoteSuccess(it!!) }
+            emitAll(remoteEditions)
+        }
+    }
+
+    protected inline fun <reified T : Any> callerItem(
         crossinline local: suspend () -> T?,
         crossinline remote: () -> Flow<ProcessState<T>>,
         crossinline onRemoteSuccess: suspend (T) -> Unit
     ): Flow<ProcessState<T>> = flow {
-
-        val localData = if (prioritizeRemote) null else getFromLocalOrNull(local)
-
-        if (localData != null) emit(localData)
-        else emitFromRemoteCall(remote, local, onRemoteSuccess)
-    }
-
-    @PublishedApi
-    internal suspend inline fun <T : Any> getFromLocalOrNull(
-        crossinline local: suspend () -> T?,
-        exception: Exception? = null
-    ): ProcessState.Success<T>? {
-        val localData: T? = local.invoke()
-        return if ((localData is List<*> && localData.isEmpty()) || (localData == null)) null
-        else localData.toSuccessProcess(exception)
-    }
-
-    @PublishedApi
-    internal suspend inline fun <T : Any> FlowCollector<ProcessState<T>>.emitFromRemoteCall(
-        crossinline remote: () -> Flow<ProcessState<T>>,
-        crossinline local: suspend () -> T?,
-        crossinline onRemoteSuccess: suspend (T) -> Unit,
-    ) {
-        emitAll(
-            remoteCall(
-                call = remote,
-                backupSourceOnFail = local,
-                onSuccess = { onRemoteSuccess(it) },
-            )
-        )
-    }
-
-    @PublishedApi
-    internal suspend inline fun <T : Any> remoteCall(
-        crossinline call: () -> Flow<ProcessState<T>>,
-        crossinline onSuccess: suspend (T) -> Unit,
-        crossinline backupSourceOnFail: suspend () -> T?
-
-    ): Flow<ProcessState<T>> = flow {
-        call.invoke().collect { process ->
-            if (process is ProcessState.Failed)
-                emit(getFromLocalOrNull(backupSourceOnFail) ?: process)
-            else {
-                emit(process)
-                if (process is ProcessState.Success) onSuccess(process.data!!)
-            }
+        val localData = local.invoke()
+        if (localData != null)
+            emit(ProcessState.Success(localData))
+        else {
+            val remoteEditions: Flow<ProcessState<T>> = remote.invoke()
+            Log.d(REMOTE_CALL, remoteEditions::class.qualifiedName!!)
+            remoteEditions.onSuccess.collect { onRemoteSuccess(it!!) }
+            emitAll(remoteEditions)
         }
     }
-
-
 }
